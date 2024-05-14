e2d0c0fb80f1533c201e9537a07c918198a339ae
BalbuenaOscarE <balbuenaoscar5@gmail.com>
2024-05-14 19:51:16 -0300

    se modificó el menu hamburguesa

--- js/js.js
+++ js/js.js
@@ -68,3 +68,14 @@ function submit() {
 
 
 
+// NAVBAR
+
+document.addEventListener("DOMContentLoaded", function() {
+    const menuBtn = document.getElementById("menuBtn");
+    const menu = document.getElementById("menu");
+  
+    menuBtn.addEventListener("click", function() {
+      menu.classList.toggle("hidden");
+    });
+  });
+
